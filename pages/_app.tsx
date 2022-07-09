1b9fde293d1f9657f783c39b522932cb7ab6572d
rafaelladi <rafael.ldietrich@gmail.com>
2022-07-09 02:31:54 -0300

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "bootstrap/dist/css/bootstrap.css";
+import type { AppProps } from "next/app";
+import {useEffect} from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>Be Connect</title>
+        </Head>
+
+        <Component {...pageProps} />
+      </>
+  )
 }
 
-export default MyApp
+export default MyApp;
